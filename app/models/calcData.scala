9a2596ff4c442c601ac496c1ac3270201ad9c869
narkaah <nenyi.arkaah@gmail.com>
2017-02-04 16:16:04 +0000

    Refactoring for cleaner code using more pattern matching.

--- app/models/calcData.scala
+++ app/models/calcData.scala
@@ -3,5 +3,5 @@ package models
 /**
   * Created by Nenyi on 17/10/2016.
   */
-case class calcData(amount: Double, interest: Double)
-case class paymentData(amount: Double, interest: Double, payment: Double)
+case class CalcData(amount: Double, interest: Double)
+case class PaymentData(amount: Double, interest: Double, payment: Double)
